2a61ff5276369107789c1191d925cca313fe102e
GraypigeonHGH <2371849349@qq.com>
2026-03-10 20:31:20 +0800

    feat: 删除贡献模块

--- web/admin/src/components/Sidebar/index.tsx
+++ web/admin/src/components/Sidebar/index.tsx
@@ -11,7 +11,6 @@ import {
   IconNeirongguanli,
   IconTongjifenxi1,
   IconJushou,
-  IconGongxian,
   IconPaperFull,
   IconDuihualishi1,
   IconChilun,
@@ -51,14 +50,6 @@ const MENUS = [
       ConstsUserKBPermission.UserKBPermissionFullControl,
       ConstsUserKBPermission.UserKBPermissionDataOperate,
     ],
-  },
-  {
-    label: '贡献',
-    value: '/contribution',
-    pathname: 'contribution',
-    icon: IconGongxian,
-    show: true,
-    perms: [ConstsUserKBPermission.UserKBPermissionFullControl],
   },
   {
     label: '问答',
